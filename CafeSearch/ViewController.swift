7a0615f1329880e5b93c90d71658ad9dd3ad180e
honda <hondayuki0606@yahoo.co.jp>
2020-10-08 01:08:21 +0900

    ローカル環境に画像登録 成功

--- CafeSearch/ViewController.swift
+++ CafeSearch/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //URL to our web service
     //let URL_SAVE_BOY = "http://[Your ip Address]/MyWebService/api/createteam.php"
     
-    let URL_SAVE_BOY = "https://ibop-asia.net/check/setagaya/input_do.php"
+    let URL_SAVE_BOY = "http://10.207.171.16:8081/laravelapp/public/input_do.php"
     var activityIndicatorView = UIActivityIndicatorView()
     //Button　action Method
     @IBOutlet weak var NameFeild: UITextField!
@@ -74,69 +74,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // アラートの表示
         present(alertController,animated: true,completion: nil)
     
-       // client.multipartPost(urlString: URL_SAVE_BOY, parameters: parameters)
-//        //created NSURL
-//          let requestURL = NSURL(string: URL_SAVE_BOY)
-//          //creating NSMutableURLRequest
-//          let request = NSMutableURLRequest(url: requestURL! as URL)
-//          //setting the method to post
-//          request.httpMethod = "POST"
-//        
-//        //テキストフィールドから値の取得
-//        let user_id: String = "test_user"
-//        let lat: Double = 49.1111
-//        let lot: Double = 98.3234
-//        let comment: String = "コメント情報"
-//        let hash_tag: String = "#カフェ"
-//        let user_tag: String = "@hondayuki"
-//        //テキストフィールドからキーと値を連結してpostパラメータを生成する
-//        let postParameters = "user_id=\(user_id)&lat=\(lat)&lot=\(lot)&comment=\(comment)&hash_tag=\(hash_tag)&user_tag=\(user_tag)"
-//        //let postParameters = "address="+addressNo+"&sex="+sexNo!;
-//
-//      //  request.body.appendData(imageData!)
-//        //ボディをリクエストするためのパラメータを追加する
-//        request.httpBody = postParameters.data(using: String.Encoding.utf8)
-//        //投稿要求を送信するタスクを作成する
-//        let task = URLSession.shared.dataTask(with: request as URLRequest){
-//        data, response, error in
-//        if error != nil {
-//        print("error is \(String(describing: error))")
-//        return;
-//        }
-//        //返ってきたJsonの解析
-//        do {
-        //NSDictionaryに変換する
-//         let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
-//        //jsonデータの解析
-//         if let parseJSON = myJSON {
-//        //stringの生成
-//         var msg : String!
-//        //jsonからのレスポンスを取得
-//         msg = parseJSON["message"] as! String?
-//        //返ってきたものを表示
-//         print(msg ?? "error")
-//         }
-//        }catch{
-//         print(error)
-//        }
-//        }
-//        task.resume()
-       // self.performSegue(withIdentifier: "AfterVC", sender: nil)
     }
-
-     
+    
     // imageを選ぶボタン
     @IBOutlet weak var liblaryBtn: UIBarButtonItem!
-    // カメラ撮影ボタン
-    @IBAction func cameraBtn(_ sender: UIBarButtonItem) {
+    @IBAction func CameraButton(_ sender: UIButton) {
         let sourceType:UIImagePickerController.SourceType = UIImagePickerController.SourceType.camera
 
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-            let picker = UIImagePickerController()
-            picker.sourceType = sourceType
-            picker.delegate = self
-            self.present(picker, animated: true)
-        }
+               if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+                   let picker = UIImagePickerController()
+                   picker.sourceType = sourceType
+                   picker.delegate = self
+                   self.present(picker, animated: true)
+               }
+    }
+    // カメラ撮影ボタン
+    @IBAction func cameraBtn2(_ sender: UIBarButtonItem) {
+
     }
     
     func imagePickerController(_ picker: UIImagePickerController,
@@ -147,7 +101,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.dismiss(animated: true)
     }
     
+    @IBAction func cameraBtn(_ sender: UIBarButtonItem) {
+        let sourceType:UIImagePickerController.SourceType = UIImagePickerController.SourceType.camera
 
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
