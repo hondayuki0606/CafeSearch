e66722afc056a6b20609d2c39f0219842da5a7af
honda <hondayuki0606@yahoo.co.jp>
2020-10-10 11:45:04 +0900

    次画面でのアップデート処理追加

--- CafeSearch/ViewController2.swift
+++ CafeSearch/ViewController2.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var recieveImage: UIImageView!
     var postImage:UIImage?
     @IBOutlet weak var imageView: UIImageView!
@@ -20,22 +20,53 @@ class ViewController2: UIViewController, UIImagePickerControllerDelegate, UINavi
     @IBOutlet weak var NameFeild: UITextField!
     @IBOutlet weak var oldFeild: UITextField!
     @IBOutlet weak var downloadBtn: UIButton!
-
     
-    @IBOutlet weak var saveBtn: UIButton!
-    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
-          
+    @IBAction func upload(_ sender: Any) {
+        
         let alertController = UIAlertController(title: "アップロード",message: "画像アップロード処理を実行してもよろしいでしょうか？", preferredStyle: UIAlertController.Style.alert)
-
+        
+        // OKボタンの実装
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
+            
+            // OKがクリックされた時の処理
+            let client = APIClient()
+            let parameters:[String: Any]  = ["user_id":  "hondayuki0606",
+                                             "lat": 23.77,
+                                             "lot": 99.222,
+                                             "comment":  "コメント情報",
+                                             "hash_tag": "#カフェ",
+                                             "user_tag": "@hondayuki0606",
+                                             "image":     self.imageView.image!]
+            // サーバー処理実施
+            client.multipartPost(urlString: self.URL_SAVE_BOY, parameters: parameters)
+            
+        }
+        
+        // CANCELボタンの実装
+        let cancelButton = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        // アップロードボタンの追加
+        alertController.addAction(okAction)
+        // CANCELボタンの追加
+        alertController.addAction(cancelButton)
+        // アラートの表示
+        present(alertController,animated: true,completion: nil)
+        
+    }
+    @IBOutlet weak var uploadBtn: UIButton!
+    @IBAction func uploadBtn(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "アップロード",message: "画像アップロード処理を実行してもよろしいでしょうか？", preferredStyle: UIAlertController.Style.alert)
+        
         // OKボタンの実装
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
             
             // OKがクリックされた時の処理
             let client = APIClient()
             let parameters:[String: Any]  = ["user_id": "hondayuki0606", "lat": 23.77, "lot": 99.222,
-                                                 "comment": "コメント情報",
-                                                 "hash_tag": "#カフェ","user_tag": "@hondayuki0606",
-                                                 "image": self.imageView.image!]
+                                             "comment": "コメント情報",
+                                             "hash_tag": "#カフェ","user_tag": "@hondayuki0606",
+                                             "image": self.imageView.image!]
             // インジケーターを表示＆アニメーション開始
             // 通信中インジケーター表示
             self.activityIndicatorView.center = self.view.center //画面中央に置く
@@ -51,26 +82,31 @@ class ViewController2: UIViewController, UIImagePickerControllerDelegate, UINavi
             client.multipartPost(urlString: self.URL_SAVE_BOY, parameters: parameters)
             
         }
- 
+        
         // CANCELボタンの実装
         let cancelButton = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler: nil)
-  
+        
         // アップロードボタンの追加
         alertController.addAction(okAction)
         // CANCELボタンの追加
         alertController.addAction(cancelButton)
         // アラートの表示
         present(alertController,animated: true,completion: nil)
+        
+    }
     
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
