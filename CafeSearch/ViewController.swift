3283a349e93ba4a3e10d897edd3454615afcf080
honda <hondayuki0606@yahoo.co.jp>
2020-10-10 12:14:17 +0900

    初期表示画面の不要なアップロード処理のコード削除 投稿画面のハッシュボタン、友達ボタン、コメントボタンを追加

--- CafeSearch/ViewController.swift
+++ CafeSearch/ViewController.swift
@@ -27,54 +27,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
         print("test")
     }
-   // UIBarButtonItem!
-    
-    
-    @IBOutlet weak var saveBtn: UIButton!
-    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
-          
-        let alertController = UIAlertController(title: "アップロード",message: "画像アップロード処理を実行してもよろしいでしょうか？", preferredStyle: UIAlertController.Style.alert)
-
-        // OKボタンの実装
-        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){ (action: UIAlertAction) in
-            
-            // OKがクリックされた時の処理
-            let client = APIClient()
-            let parameters:[String: Any]  = ["user_id": "hondayuki0606", "lat": 23.77, "lot": 99.222,
-                                                 "comment": "コメント情報",
-                                                 "hash_tag": "#カフェ","user_tag": "@hondayuki0606",
-                                                 "image": self.imageView.image!]
-            // インジケーターを表示＆アニメーション開始
-            // 通信中インジケーター表示
-            self.activityIndicatorView.center = self.view.center //画面中央に置く
-            self.activityIndicatorView.style = .whiteLarge //スタイル設定。他に、gray,whiteが選べる
-            self.activityIndicatorView.color = .gray //色設定
-            self.view.addSubview(self.activityIndicatorView) //viewに追加する
-            self.activityIndicatorView.startAnimating() //ローディング(くるくる)をまわす
-            self.activityIndicatorView.hidesWhenStopped = true //ローディング止まった時、indicator消す(隠す)
-            if self.activityIndicatorView.isAnimating { //ローディングしてるか確認
-                print("ロード中")
-            }
-            // サーバー処理実施
-            client.multipartPost(urlString: self.URL_SAVE_BOY, parameters: parameters)
-            
-            // インジケーターを非表示＆アニメーション終了
-            self.activityIndicatorView.stopAnimating() //ローディング止める
-            
- //           performSegue(withIdentifier: "goNext", sender: nil)
-        }
- 
-        // CANCELボタンの実装
-        let cancelButton = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler: nil)
-  
-        // アップロードボタンの追加
-        alertController.addAction(okAction)
-        // CANCELボタンの追加
-        alertController.addAction(cancelButton)
-        // アラートの表示
-        present(alertController,animated: true,completion: nil)
-    
-    }
     
     // imageを選ぶボタン
     @IBOutlet weak var liblaryBtn: UIBarButtonItem!
